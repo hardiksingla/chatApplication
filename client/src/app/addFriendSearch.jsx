7890b8d8768a50339f2a3316e7db1bad0463db23
hardiksingla <07hardiksingla@gmail.com>
2024-02-19 12:29:23 +0530

    server url

--- client/src/app/addFriendSearch.jsx
+++ client/src/app/addFriendSearch.jsx
@@ -21,7 +21,7 @@ function AddFriendSearch(){
     e.preventDefault()
     const names = document.getElementById("names")
     names.style.display = names.style.display == "flex" ? "none" : "flex"
-    const response = await fetch(`${SERVER_URL}/api/api/search`,
+    const response = await fetch(`${SERVER_URL}/api/search`,
     // const response = await fetch("http://localhost:3000/api/search",
     {method: "POST",
     headers: {"Content-Type": "application/json"},
